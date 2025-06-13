cfe8f7b499f7eb0ef9cb46a4ec808af7abed6c67
vishavjeet1999 <vishavjeetsharma88@gmail.com>
2025-06-13 18:17:52 +0530

    fix

--- App.tsx
+++ App.tsx
@@ -4,17 +4,31 @@
  *
  * @format
  */
-
+import {useEffect} from 'react'
 import { NewAppScreen } from '@react-native/new-app-screen';
 import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
 
+import CallDetector from "react-native-scizers-call-detector"
+
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
+
+  useEffect(()=>{
+    // checkPhoneState()
+    // console.log(CallDetector,"test......")
+  },[])
+
+  // const checkPhoneState = ()=>{
+  //   let state = CallDetector.CheckPhoneState((state)=>{
+  //     console.log(state,"state here............")
+  //   })
+  // }
 
   return (
     <View style={styles.container}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <NewAppScreen templateFileName="App.tsx" />
+      
     </View>
   );
 }
